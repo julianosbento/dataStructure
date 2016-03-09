38ce0ee710e3b22a285e20804847efe0057690b1
Julianosantosb <js.bentocontato@gmail.com>
2016-03-09 10:28:45 -0300

    pilhaBalanceamento.py

---
+++ pilhaBalanceamento.py
@@ -0,0 +1,89 @@
+import unittest
+
+
+class Pilha ():
+    def __init__(self):
+        self.lista = []
+
+    def vazia (self):
+        return bool(self.lista)
+
+    def topo (self):
+        if self.lista:
+            return self.lista [-1]
+
+        raise PilhaVaziaErro ()
+
+    def empilhar(self, valor):
+        return self.lista.append(valor)
+
+    def desempilhar (self):
+        try:
+            return self.lista.pop()
+        except:
+            raise PilhaVaziaErro()
+
+    def esta_balanceada (word):
+        nada = ''
+
+        for letters in word:
+            if letters == '':
+                .vazia()
+
+    """
+    Função que calcula se expressão possui parenteses, colchetes e chaves balanceados
+    O Aluno deverá informar a complexidade de tempo e espaço da função
+    Deverá ser usada como estrutura de dados apenas a pilha feita na aula anterior
+    :param expressao: string com expressao a ser balanceada
+    :return: boleano verdadeiro se expressao está balanceada e falso caso contrário
+    """
+    pass
+
+
+class BalancearTestes(unittest.TestCase):
+    def test_expressao_vazia(self):
+        self.assertTrue(esta_balanceada(''))
+
+    def test_parenteses(self):
+        self.assertTrue(esta_balanceada('()'))
+
+    def test_chaves(self):
+        self.assertTrue(esta_balanceada('{}'))
+
+    def test_colchetes(self):
+        self.assertTrue(esta_balanceada('[]'))
+
+    def test_todos_caracteres(self):
+        self.assertTrue(esta_balanceada('({[]})'))
+        self.assertTrue(esta_balanceada('[({})]'))
+        self.assertTrue(esta_balanceada('{[()]}'))
+
+    def test_chave_nao_fechada(self):
+        self.assertFalse(esta_balanceada('{'))
+
+    def test_colchete_nao_fechado(self):
+        self.assertFalse(esta_balanceada('['))
+
+    def test_parentese_nao_fechado(self):
+        self.assertFalse(esta_balanceada('('))
+
+    def test_chave_nao_aberta(self):
+        self.assertFalse(esta_balanceada('}{'))
+
+    def test_colchete_nao_aberto(self):
+        self.assertFalse(esta_balanceada(']['))
+
+    def test_parentese_nao_aberto(self):
+        self.assertFalse(esta_balanceada(')('))
+
+    def test_falta_de_caracter_de_fechamento(self):
+        self.assertFalse(esta_balanceada('({[]}'))
+
+    def test_falta_de_caracter_de_abertura(self):
+        self.assertFalse(esta_balanceada('({]})'))
+
+    def test_expressao_matematica_valida(self):
+        self.assertTrue(esta_balanceada('({[1+3]*5}/7)+9'))
+
+    def test_char_errado_fechando(self):
+        self.assertFalse(esta_balanceada('[)'))
